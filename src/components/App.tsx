1ce8bf68c888c7bed67b972726703ef70638af2d
Adam Aaronson <adamaaronson@gmail.com>
2023-02-09 23:38:51 -0600

    Get image rendering and downloading working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
+import * as htmlToImage from 'html-to-image';
 import '../styles/App.scss'
+
+const TEXT_STROKE_STEPS = 32;
+const APP_TITLE = "Whisperer"
 
 function App() {
     let [imageText, setImageText] = useState('');
@@ -12,14 +16,47 @@ function App() {
         setImage(image);
         setHasUploaded(true);
         setImageObjectUrl(URL.createObjectURL(image))
+        setCanDownload(true);
+    }
+
+    const downloadImage = () => {
+        htmlToImage.toCanvas(document.getElementById('image-output')!)
+        .then(function (dataUrl) {
+            const link = document.createElement('a');
+            link.download = 'whisperer-image';
+            link.href = dataUrl.toDataURL();
+            link.click();
+        });
+    }
+
+    const getTextShadowCoordinates = (steps: number, strokeWidth: number) => {
+        let coordinates = []
+        
+        for (let i = 0; i < steps; i++) {
+            let angle = (i * 2 * Math.PI) / steps;
+            let cos = Math.round(10000 * Math.cos(angle)) / 10000;
+            let sin = Math.round(10000 * Math.sin(angle)) / 10000;
+            coordinates.push({
+                "x": cos * strokeWidth,
+                "y": sin * strokeWidth
+            })
+        }
+
+        return coordinates;
     }
 
     return (
         <div className="app">
             <header>
-                <h1 className="title">
-                    Whisperer
-                </h1>
+                <div className="title-wrapper">
+                    <h1 className="title">
+                        {APP_TITLE}
+                    </h1>
+                </div>
+                <h2 className="subtitle">
+                    create <a href="https://knowyourmeme.com/memes/sites/whisper" target="_blank" rel="noopener noreferrer">whisper</a><br/>
+                    images online
+                </h2>
             </header>
             <section className="settings-section">
                 <div className="text-field-wrapper">
@@ -39,9 +76,23 @@ function App() {
                         Select image:
                     </div>
                     { hasUploaded ?
-                        <div className="image-canvas-wrapper">
-                            <canvas className="image-canvas" />
-                            <img src={imageObjectUrl} width="100%" />
+                        <div className="image-output-wrapper">
+                            <div className="image-output" id="image-output">
+                                <div className="image-text">
+                                    {imageText}
+                                </div>
+                                { getTextShadowCoordinates(TEXT_STROKE_STEPS, 0.06).map(coord =>
+                                    <div className="image-text-evil" style={{
+                                        "left": coord.x + "em",
+                                        "top": coord.y + "em"
+                                    }}>
+                                        {imageText}
+                                    </div>
+                                ) }
+                                <div className="image-image">
+                                    <img src={imageObjectUrl} width="100%" />
+                                </div>
+                            </div>
                         </div>
                     :
                         <>
@@ -62,11 +113,8 @@ function App() {
                     
                 </div>
             </section>
-            <section className="output-section">
-                <canvas className="image-canvas">
-
-                </canvas>
-                <button className="download-image-button" disabled={!canDownload}>
+            <section className="download-section">
+                <button className="download-image-button" disabled={!canDownload} onClick={() => downloadImage()}>
                     Download image
                 </button>
             </section>
