e832cbedde44ed3686fb5176cb8e032a784a9b1a
Adam Aaronson <adamaaronson@gmail.com>
2023-02-11 20:30:39 -0600

    Add font choices

--- src/components/ImageOutput.tsx
+++ src/components/ImageOutput.tsx
@@ -11,6 +11,8 @@ interface Props {
 
 export function ImageOutput({ imageText, imageObjectUrl, onChangeImage }: Props) {
     const [fontSize, setFontSize] = useState("1")
+    const [font, setFont] = useState("Whisper")
+    const [outline, setOutline] = useState(true)
     const [textColor, setTextColor] = useState("#fafafa")
     const [outlineColor, setOutlineColor] = useState("#111")
 
@@ -41,16 +43,18 @@ export function ImageOutput({ imageText, imageObjectUrl, onChangeImage }: Props)
         <div className="image-output" id="image-output">
             <div className="image-text-wrapper">
                 <div className="image-text" style={{
-                    "color": textColor,
+                    color: textColor,
+                    fontFamily: font,
                     ...getFontSize()
                 }}>
                     {imageText}
                 </div>
-                { getTextShadowCoordinates(TEXT_STROKE_STEPS, 0.06).map(coord =>
+                { outline && getTextShadowCoordinates(TEXT_STROKE_STEPS, 0.06).map(coord =>
                     <div className="image-text-evil" key={`${coord.x}, ${coord.y}`} style={{
-                        "left": coord.x + "em",
-                        "top": coord.y + "em",
-                        "color": outlineColor,
+                        left: coord.x + "em",
+                        top: coord.y + "em",
+                        color: outlineColor,
+                        fontFamily: font,
                         ...getFontSize()
                     }}>
                         {imageText}
@@ -64,13 +68,13 @@ export function ImageOutput({ imageText, imageObjectUrl, onChangeImage }: Props)
         <div className="settings-wrapper">
             <div className="settings-item">
                 <div className="settings-item-name">
-                    Font size
+                    Font size:
                 </div>
                 <input
-                    className="font-size-slider"
+                    className="settings-item-input"
                     type="range"
                     min="0.25"
-                    max="3"
+                    max="5"
                     step="0.05"
                     onChange={(e) => {setFontSize(e.target.value)}}
                     defaultValue={fontSize}
@@ -78,9 +82,36 @@ export function ImageOutput({ imageText, imageObjectUrl, onChangeImage }: Props)
             </div>
             <div className="settings-item">
                 <div className="settings-item-name">
-                    Text color
+                    Font:
+                </div>
+                <select
+                    className="settings-item-input"
+                    value={font}
+                    onChange={(e) => {setFont(e.target.value)}}
+                >
+                    <option value="Whisper">Upright (default)</option>
+                    <option value="Arial">Arial</option>
+                    <option value="Courier New">Courier New</option>
+                    <option value="Times New Roman">Times New Roman</option>
+                </select>
+            </div>
+            <div className="settings-item">
+                <span className="settings-item-name">
+                    Outline:
+                </span>
+                <input
+                    className="settings-item-input"
+                    type="checkbox"
+                    checked={outline}
+                    onClick={() => setOutline(!outline)}
+                />
+            </div>
+            <div className="settings-item">
+                <div className="settings-item-name">
+                    Text color:
                 </div>
                 <input
+                    className="settings-item-input"
                     type="color"
                     value={textColor}
                     onChange={(e) => {setTextColor(e.target.value)}}
@@ -88,9 +119,10 @@ export function ImageOutput({ imageText, imageObjectUrl, onChangeImage }: Props)
             </div>
             <div className="settings-item">
                 <div className="settings-item-name">
-                    Outline color
+                    Outline color:
                 </div>
                 <input
+                    className="settings-item-input"
                     type="color"
                     value={outlineColor}
                     onChange={(e) => {setOutlineColor(e.target.value)}}
