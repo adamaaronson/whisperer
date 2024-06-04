43ecbf6b0b0336aa5bf0f58cf238444ae5c74e12
Adam Aaronson <adamaaronson@gmail.com>
2024-06-04 17:57:01 -0400

    Restructure header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
-import '../styles/Header.scss'
+import "../styles/Header.scss";
 
 export function Header() {
-    return <header className="app-header">
-        <div className="title-wrapper">
-            <h1 className="title">
-                Whisperer
-            </h1>
-        </div>
-        <h2 className="subtitle">
-            create your own<br/>
-            <a href="https://knowyourmeme.com/memes/sites/whisper" target="_blank" rel="noopener noreferrer">whisper</a> images
-        </h2>
-    </header>
+    return (
+        <header className="app-header">
+            <div>
+                <h1 className="title">Whisperer</h1>
+            </div>
+            <div>
+                <h2 className="subtitle">
+                    create your own
+                    <br />
+                    <a
+                        href="https://knowyourmeme.com/memes/sites/whisper"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        whisper
+                    </a>{" "}
+                    images
+                </h2>
+            </div>
+        </header>
+    );
 }
