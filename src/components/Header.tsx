c0ef24cc6fc184e7a3b8ad40f4bb803c2f26d932
Adam Aaronson <adamaaronson@gmail.com>
2024-06-04 18:00:55 -0400

    Reformat header again

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,19 @@ import "../styles/Header.scss";
 export function Header() {
     return (
         <header className="app-header">
-            <div>
-                <h1 className="title">Whisperer</h1>
-            </div>
-            <div>
-                <h2 className="subtitle">
-                    create your own
-                    <br />
-                    <a
-                        href="https://knowyourmeme.com/memes/sites/whisper"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        whisper
-                    </a>{" "}
-                    images
-                </h2>
-            </div>
+            <h1 className="title">Whisperer</h1>
+            <h2 className="subtitle">
+                create your own
+                <br />
+                <a
+                    href="https://knowyourmeme.com/memes/sites/whisper"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    whisper
+                </a>{" "}
+                images
+            </h2>
         </header>
     );
 }
