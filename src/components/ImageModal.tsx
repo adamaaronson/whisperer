c97e336064338f50f8fd93be6e85871d151108b7
Adam Aaronson <adamaaronson@gmail.com>
2024-12-20 12:44:12 -0500

    Add link to feedback survey

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -1,29 +1,52 @@
-import '../styles/ImageModal.scss';
+import "../styles/ImageModal.scss";
 
 interface Props {
-    imageUrl: string
-    onCloseModal: () => void
+    imageUrl: string;
+    onCloseModal: () => void;
 }
 
 export function ImageModal({ imageUrl, onCloseModal }: Props) {
-    return <div className="image-modal-wrapper" onClick={onCloseModal}>
-        <div className="image-modal" onClick={(e: React.MouseEvent<HTMLElement>) => e.stopPropagation()}>
-            <div className="image-modal-header">
-                <div className="image-modal-header-text">
-                    <h4 className="image-modal-title">
-                        here is your whisper:
-                    </h4>
-                    <h5 className="image-modal-subtitle">
-                        save it, or it'll disappear when you close this window
-                    </h5>
+    return (
+        <div className="image-modal-wrapper" onClick={onCloseModal}>
+            <div
+                className="image-modal"
+                onClick={(e: React.MouseEvent<HTMLElement>) =>
+                    e.stopPropagation()
+                }
+            >
+                <div className="image-modal-header">
+                    <div className="image-modal-header-text">
+                        <h4 className="image-modal-title">
+                            here is your whisper:
+                        </h4>
+                        <h5 className="image-modal-subtitle">
+                            save it, or it'll disappear when you close this
+                            window
+                        </h5>
+                    </div>
+                    <button
+                        className="image-modal-close-button"
+                        onClick={onCloseModal}
+                    >
+                        ×
+                    </button>
                 </div>
-                <button className="image-modal-close-button" onClick={onCloseModal}>
-                    ×
-                </button>
-            </div>
-            <div className="image-modal-output">
-                <img className="image-modal-output-image" src={imageUrl} alt="whisper" crossOrigin="anonymous" />
+                <div className="image-modal-output">
+                    <img
+                        className="image-modal-output-image"
+                        src={imageUrl}
+                        alt="whisper"
+                        crossOrigin="anonymous"
+                    />
+                </div>
+                <div className="image-modal-footer">
+                    got feedback? fill out this{" "}
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPumIVFRfFFLpDfTZDMXdArtbP4M6WQDJX1PxO7wQBLAyq0w/viewform">
+                        quick survey
+                    </a>
+                    !
+                </div>
             </div>
         </div>
-    </div>
+    );
 }
