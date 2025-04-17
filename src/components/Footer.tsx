7d1643023c3e8cf9b3ea95d1158ae0442d1d3aad
Adam Aaronson <adamaaronson@gmail.com>
2025-04-17 17:23:43 -0400

    Fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,35 @@
-import '../styles/Footer.scss'
+import "../styles/Footer.scss";
 
 export function Footer() {
-    return <footer>
-        made by <a href="https://twitter.com/aaaronson" target="_blank" rel="noopener noreferrer">Adam Aaronson</a><br/>
-        thanks to <a href="https://twitter.com/anniierau" target="_blank" rel="noopener noreferrer">Annie Rauwerda</a>'s idea<br/>
-        not affiliated with <a href="https://whisper.sh" target="_blank" rel="noopener noreferrer">whisper</a>
-    </footer>
+    return (
+        <footer>
+            made by{" "}
+            <a
+                href="https://instagram.com/adama47/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Adam Aaronson
+            </a>
+            <br />
+            thanks to{" "}
+            <a
+                href="https://www.instagram.com/annierau/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Annie Rauwerda
+            </a>
+            's idea
+            <br />
+            not affiliated with{" "}
+            <a
+                href="https://whisper.sh"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                whisper
+            </a>
+        </footer>
+    );
 }
